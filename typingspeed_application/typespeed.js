712668776d0e917ae133f17929a525aab873f1d3
shubhamjoshigithub <shubhamjoshii1525@gmail.com>
2025-03-21 09:31:07 +0530

    first commit of typespeed app html css js files

--- typingspeed_application/typespeed.js
+++ typingspeed_application/typespeed.js
@@ -24,78 +24,87 @@ function getAndAppendQuote() {
 }
 */
 
-let getAndAppendQuote = async() => {
-    let url = "https://apis.ccbp.in/random-quote";
-    let options = {
-        method: "GET",
-    };
-    let response = await fetch(url, options);
-    let jsonData = await response.json();
-    quoteDisplay.textContent = jsonData.content;
-}
+let getAndAppendQuote = async () => {
+  let url = "https://apis.ccbp.in/random-quote";
+  let options = {
+    method: "GET",
+  };
+  let response = await fetch(url, options);
+  let jsonData = await response.json();
+  quoteDisplay.textContent = jsonData.content;
+};
 
 getAndAppendQuote();
 
-quoteDisplay.addEventListener("copy", function() {
-    quoteInput.placeholder = "No cheating son!!";
-    getAndAppendQuote();
+quoteDisplay.addEventListener("copy", function () {
+  quoteInput.placeholder = "No cheating please!!";
+  getAndAppendQuote();
 });
 
-quoteInput.addEventListener("click", function() {
-    quoteInput.placeholder = "Type Here!!";
+quoteInput.addEventListener("click", function () {
+  quoteInput.placeholder = "Type Here!!";
 });
-
 
 function timeGo() {
-    timeClockId = setInterval(function() {
-        let count = parseInt(timer.textContent);
-        timer.textContent = count + 1;
-    }, 1000);
+  timeClockId = setInterval(function () {
+    let count = parseInt(timer.textContent);
+    timer.textContent = count + 1;
+  }, 1000);
 }
 
 timeGo();
 
-timeReset.addEventListener("click", function() {
-    quoteInput.value = "";
-    clearInterval(timeClockId);
-    timer.textContent = 0;
-    timeGo();
+timeReset.addEventListener("click", function () {
+  quoteInput.value = "";
+  clearInterval(timeClockId);
+  timer.textContent = 0;
+  timeGo();
 });
 
-submitBtn.addEventListener("click", function() {
-    let testQuote = quoteDisplay.textContent;
-    let answerQuote = quoteInput.value;
-    if (testQuote === answerQuote) {
-        result.textContent = "You typed in " + timer.textContent + " seconds";
-        clearInterval(timeClockId);
-    } else {
-        result.textContent = "You typed incorrect sentence";
+submitBtn.addEventListener("click", function () {
+  let testQuote = quoteDisplay.textContent;
+  let answerQuote = quoteInput.value;
+  if (testQuote === answerQuote) {
+    result.textContent = "You typed in " + timer.textContent + " seconds";
+    clearInterval(timeClockId);
+  } else {
+    result.textContent = "You typed incorrect sentence";
+  }
+});
+
+resetBtn.addEventListener("click", function () {
+  clearInterval(timeClockId);
+  displayTab.classList.add("d-none");
+  speedTypingTest.classList.add(
+    "temp-height",
+    "d-flex",
+    "align-items-center",
+    "justify-content-center"
+  );
+
+  let spinner = document.getElementById("spinner");
+  speedTypingTest.appendChild(spinner);
+  spinner.classList.toggle("d-none");
+
+  getAndAppendQuote();
+  let spincount = 0;
+  let spinId = setInterval(function () {
+    spincount += 1;
+    if (spincount === 1) {
+      clearInterval(spinId);
+      spinner.classList.toggle("d-none");
+      displayTab.classList.remove("d-none");
+      speedTypingTest.classList.remove(
+        "temp-height",
+        "d-flex",
+        "align-items-center",
+        "justify-content-center"
+      );
+      quoteInput.value = "";
+      result.textContent = "";
+      timer.textContent = 0;
     }
-});
+  }, 500);
 
-resetBtn.addEventListener("click", function() {
-    clearInterval(timeClockId);
-    displayTab.classList.add("d-none");
-    speedTypingTest.classList.add("temp-height", "d-flex", "align-items-center", "justify-content-center");
-
-    let spinner = document.getElementById("spinner");
-    speedTypingTest.appendChild(spinner);
-    spinner.classList.toggle("d-none");
-
-    getAndAppendQuote();
-    let spincount = 0;
-    let spinId = setInterval(function() {
-        spincount += 1;
-        if (spincount === 1) {
-            clearInterval(spinId);
-            spinner.classList.toggle("d-none");
-            displayTab.classList.remove("d-none");
-            speedTypingTest.classList.remove("temp-height", "d-flex", "align-items-center", "justify-content-center");
-            quoteInput.value = "";
-            result.textContent = "";
-            timer.textContent = 0;
-        }
-    }, 500);
-
-    timeGo();
+  timeGo();
 });
